0ae439f339fe70a66a05a6a222bd4460ba27c9ea
Everton Luiz Pereira <tom.msn@hotmail.com>
2014-07-15 21:12:56 -0300

    Change Project Name

---
+++ Chapter07/Lesson1/Starting Multiple Threads/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Starting_Multiple_Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Using the ThreadPool Class
+
+            ThreadPool.QueueUserWorkItem(ThreadProc1);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Main thread exits.");
+
+
+            string state = "Hello, world!";
+            ThreadPool.QueueUserWorkItem(ThreadProc, state);
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Main thread exits.");
+
+
+            //By default, the thread pool has 250 worker threads per available processor
+            ThreadPool.QueueUserWorkItem(ThreadProc, "Thread 1");
+            ThreadPool.QueueUserWorkItem(ThreadProc, "Thread 2");
+            ThreadPool.QueueUserWorkItem(ThreadProc, "Thread 3");
+            ThreadPool.QueueUserWorkItem(ThreadProc, "Thread 4");
+
+
+            int workerThreads;
+            int completionPortThreads;
+            ThreadPool.GetAvailableThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine("Available threads: " + workerThreads.ToString());
+
+            #endregion
+
+            #region Understanding Foreground and Background Threads
+
+            ThreadPool.QueueUserWorkItem(ThreadProc2, "Thread 1");
+            ThreadPool.QueueUserWorkItem(ThreadProc2, "Thread 2");
+            ThreadProc2("Thread 3"); // Called as part of the foreground thread
+            ThreadPool.QueueUserWorkItem(ThreadProc2, "Thread 4");
+            Console.WriteLine("Main thread does some work, then sleeps.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Main thread exits.");
+            Console.ReadKey();
+
+            #endregion
+        }
+        static void ThreadProc1(Object stateInfo)
+        {
+            Console.WriteLine("Hello from the thread pool.");
+        }
+
+        // You must manually cast from the Object class in C#
+        static void ThreadProc(Object stateInfo)
+        {
+            string state = (string)stateInfo;
+            Console.WriteLine("Hello from the thread pool: " + state);
+        }
+        static void ThreadProc2(Object stateInfo)
+        {
+            string state = (string)stateInfo;
+            if (Thread.CurrentThread.IsBackground)
+                Console.WriteLine("Hello from backgroud thread: " + state);
+            else
+                Console.WriteLine("Hello from foreground thread: " + state);
+        }
+    }
+}
---
+++ Chapter07/Lesson2/Managing Threads/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Managing_Threads
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Starting and Stopping Threads
+
+            // Create the thread object, passing in the
+            // DoWork method using a ThreadStart delegate.
+            Thread DoWorkThread = new Thread(new ThreadStart(DoWork));
+            // Start the thread.
+            DoWorkThread.Start();
+            // Wait one second to allow the thread to begin to run
+            Thread.Sleep(1000);
+            // Abort the thread
+            DoWorkThread.Abort();
+            Console.WriteLine("The Main() thread is ending.");
+            Thread.Sleep(6000);
+
+            #endregion
+
+            #region Passing Data to and from Threads
+
+            // Supply the state information required by the task.
+            Multiply m = new Multiply("Hello, world!", 13, new Managing_Threads.Multiply.ResultDelegate(ResultCallback));
+            Thread t = new Thread(new ThreadStart(m.ThreadProc));
+            t.Start();
+            Console.WriteLine("Main thread does some work, then waits.");
+            t.Join();
+            Console.WriteLine("Thread completed.");
+            Console.ReadKey();
+
+            #endregion
+        }
+
+        public static void DoWork()
+        {
+            Console.WriteLine("DoWork is running on another thread.");
+            try
+            {
+                Thread.Sleep(5000);
+            }
+
+            catch (ThreadAbortException ex)
+            {
+                Console.WriteLine("DoWork was aborted." + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Use finally to close all open resources.");
+            }
+
+            Console.WriteLine("DoWork has ended.");
+        }
+        public static void ResultCallback(int retValue)
+        {
+            Console.WriteLine("Returned value: {0}", retValue);
+        }
+
+
+
+    }
+    public class Multiply
+    {
+        // Delegate that defines the signature for the callback method.
+        public delegate void ResultDelegate(int value);
+
+        // State information used in the task.
+        private string greeting;
+        private int value;
+
+        // Delegate used to execute the callback method when the task is complete.
+        private ResultDelegate callback;
+
+        // The constructor obtains the state information and the callback delegate.
+        public Multiply(string _greeting, int _value, ResultDelegate _callback)
+        {
+            greeting = _greeting;
+            value = _value;
+            callback = _callback;
+        }
+        // The thread procedure performs the tasks (displaying
+        // the greeting and multiplying the value by 2).
+        public void ThreadProc()
+        {
+            Console.WriteLine(greeting);
+            if (callback != null)
+                callback(value * 2);
+        }
+    }
+}
